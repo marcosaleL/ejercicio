17105d26a849c112b53c1978954fe65013e800f3
Marcos Alejandro Lazarte <mlazarte.globallogic@bancochile.cl>
2023-09-04 18:00:07 -0300

    feat: agrega response DTO

---
+++ src/main/java/com/marcos/lazarte/ejercicio/model/DTO/ResponseSignUpDTO.java
@@ -0,0 +1,38 @@
+package com.marcos.lazarte.ejercicio.model.DTO;
+
+import com.marcos.lazarte.ejercicio.model.entities.PhoneEntity;
+import com.marcos.lazarte.ejercicio.model.entities.UserEntity;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseSignUpDTO {
+
+    private Long userId;
+    private String name;
+    private String email;
+    private String password;
+    private List<PhoneEntity> phones;
+    private String created;
+    private String lastLogin;
+    private String token;
+    private boolean isActive;
+
+    public ResponseSignUpDTO(UserEntity user) {
+        this.userId = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.phones = user.getPhones();
+        this.created = user.getCreated();
+        this.lastLogin = user.getLastLogin();
+        this.isActive = true;
+    }
+
+}
